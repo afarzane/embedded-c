e91fc5e0ac4dad6877199c473a70de44f281841b
afarzane <afarzane@sfu.ca>
2023-12-27 14:59:56 -0800

    Designed an assembly project from a flow chart - flow chart is included as a .png in the main project folder

---
+++ Asm_Flow_DesignFromFlowChart/main.asm
@@ -0,0 +1,67 @@
+;-------------------------------------------------------------------------------
+; MSP430 Assembler Code Template for use with TI Code Composer Studio
+;
+;
+;-------------------------------------------------------------------------------
+            .cdecls C,LIST,"msp430.h"       ; Include device header file
+            
+;-------------------------------------------------------------------------------
+            .def    RESET                   ; Export program entry-point to
+                                            ; make it known to linker.
+;-------------------------------------------------------------------------------
+            .text                           ; Assemble into program memory.
+            .retain                         ; Override ELF conditional linking
+                                            ; and retain current section.
+            .retainrefs                     ; And retain any sections that have
+                                            ; references to current section.
+
+;-------------------------------------------------------------------------------
+RESET       mov.w   #__STACK_END,SP         ; Initialize stackpointer
+StopWDT     mov.w   #WDTPW|WDTHOLD,&WDTCTL  ; Stop watchdog timer
+
+
+;-------------------------------------------------------------------------------
+; Main loop here
+;-------------------------------------------------------------------------------
+
+main:
+			mov.w	Var1,	R4
+			add.w	Var2,	R4
+
+			jnc		NoCarry
+
+; If there is a carry, move into this section
+Carry:
+			mov.w	#0,	Fit
+			jmp		Done	; Helps loop back to main
+
+; If there is no carry, jump here and loop back
+NoCarry:
+			mov.w	#1,	Fit
+
+Done:
+			jmp		main
+
+;-------------------------------------------------------------------------------
+; Memory Allocation
+;-------------------------------------------------------------------------------
+
+			.data
+			.retain
+
+Var1:		.space	2
+Var2:		.space	2
+Fit:		.space	2
+
+;-------------------------------------------------------------------------------
+; Stack Pointer definition
+;-------------------------------------------------------------------------------
+            .global __STACK_END
+            .sect   .stack
+            
+;-------------------------------------------------------------------------------
+; Interrupt Vectors
+;-------------------------------------------------------------------------------
+            .sect   ".reset"                ; MSP430 RESET Vector
+            .short  RESET
+            
