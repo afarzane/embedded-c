f946f20c0375287cf15d7e2dfe076c689739ac3c
afarzane <afarzane@sfu.ca>
2023-12-27 13:20:57 -0800

    Multiply and divide using rotate

---
+++ Asm_ALU_MUL2_DIV2/main.asm
@@ -0,0 +1,67 @@
+;-------------------------------------------------------------------------------
+; MSP430 Assembler Code Template for use with TI Code Composer Studio
+;
+;
+;-------------------------------------------------------------------------------
+            .cdecls C,LIST,"msp430.h"       ; Include device header file
+            
+;-------------------------------------------------------------------------------
+            .def    RESET                   ; Export program entry-point to
+                                            ; make it known to linker.
+;-------------------------------------------------------------------------------
+            .text                           ; Assemble into program memory.
+            .retain                         ; Override ELF conditional linking
+                                            ; and retain current section.
+            .retainrefs                     ; And retain any sections that have
+                                            ; references to current section.
+
+;-------------------------------------------------------------------------------
+RESET       mov.w   #__STACK_END,SP         ; Initialize stackpointer
+StopWDT     mov.w   #WDTPW|WDTHOLD,&WDTCTL  ; Stop watchdog timer
+
+
+;-------------------------------------------------------------------------------
+; Main loop here
+;-------------------------------------------------------------------------------
+
+main:
+			mov.b	#25,	R4
+			clrc							; Clear the carry
+			rla.b	R4						; Multiply by 2 = 50
+			rla.b	R4						; Multiply by 2 = 100
+			rla.b	R4						; Multiply by 2 = 200
+
+			mov.b	#224,	R4
+			clrc							; Clear the carry
+			rrc.b	R4						; Divide by 2 = 112
+			clrc							; Clear the carry each time so that it doesn't
+			rrc.b	R4						; Divide by 2 = 56
+			clrc
+			rrc.b	R4						; And so on..
+			clrc
+			rrc.b	R4
+			clrc
+			rrc.b	R4
+			clrc
+			rrc.b	R4
+			clrc
+			rrc.b	R4
+			clrc
+			rrc.b	R4						; Rotate right 8 times to get it into the carry
+			clrc
+			rrc.b	R4						; Rotate right again to make sure it doesnt rotate around
+
+			jmp		main
+
+;-------------------------------------------------------------------------------
+; Stack Pointer definition
+;-------------------------------------------------------------------------------
+            .global __STACK_END
+            .sect   .stack
+            
+;-------------------------------------------------------------------------------
+; Interrupt Vectors
+;-------------------------------------------------------------------------------
+            .sect   ".reset"                ; MSP430 RESET Vector
+            .short  RESET
+            
