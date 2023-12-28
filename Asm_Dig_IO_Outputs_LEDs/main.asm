7fcc3a066cc600651f368b7740ab6c1e064c2065
afarzane <afarzane@sfu.ca>
2023-12-28 09:26:44 -0800

    Learned to use I/O addresses to use on board ports

---
+++ Asm_Dig_IO_Outputs_LEDs/main.asm
@@ -0,0 +1,48 @@
+;-------------------------------------------------------------------------------
+; MSP430 Assembler Code Template for use with TI Code Composer Studio
+;
+;
+;-------------------------------------------------------------------------------
+            .cdecls C,LIST,"msp430.h"       ; Include device header file
+            
+;-------------------------------------------------------------------------------
+            .def    RESET                   ; Export program entry-point to
+                                            ; make it known to linker.
+;-------------------------------------------------------------------------------
+            .text                           ; Assemble into program memory.
+            .retain                         ; Override ELF conditional linking
+                                            ; and retain current section.
+            .retainrefs                     ; And retain any sections that have
+                                            ; references to current section.
+
+;-------------------------------------------------------------------------------
+RESET       mov.w   #__STACK_END,SP         ; Initialize stackpointer
+StopWDT     mov.w   #WDTPW|WDTHOLD,&WDTCTL  ; Stop watchdog timer
+
+
+;-------------------------------------------------------------------------------
+; Main loop here
+;-------------------------------------------------------------------------------
+
+init:
+			bis.b	#BIT0,	&P1DIR			; Set P1DIR(0) to OUTPUT
+			bic.b	#LOCKLPM5,	&PM5CTL0	; Turn on Digital I/O
+
+main:
+			bis.b	#BIT0,	&P1OUT			; Turn on LED 1
+			bic.b	#BIT0,	&P1OUT			; Turn off LED 1
+
+			jmp		main
+
+;-------------------------------------------------------------------------------
+; Stack Pointer definition
+;-------------------------------------------------------------------------------
+            .global __STACK_END
+            .sect   .stack
+            
+;-------------------------------------------------------------------------------
+; Interrupt Vectors
+;-------------------------------------------------------------------------------
+            .sect   ".reset"                ; MSP430 RESET Vector
+            .short  RESET
+            
